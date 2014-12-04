b4c1ab64408aa6607ec2d7983e2d95c33433c242
Per-Kristian Helland <perkristianhelland@gmail.com>
2014-12-04 14:26:46 +0100

    First commit

---
+++ AzureCiberDemo3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureCiberDemo3.MvcApplication" Language="C#" %>
---
+++ AzureCiberDemo3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureCiberDemo3.Startup))]
+namespace AzureCiberDemo3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
